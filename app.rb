e4bd9cf8ceb894112f30f81a51528a686756606e
grahk93 <grace.juster@flatironschool.com>
2016-06-28 15:46:40 -0400

    Done.

--- app.rb
+++ app.rb
@@ -6,12 +6,15 @@ class App < Sinatra::Base
     erb :reverse
   end
 
-  post '/reverse' do
+  post '/reverse' do #because were in post we got the result of reverse, params
+    original_string = params["string"]
+    @reversed_string = original_string.reverse #save it to an instance to pass
     erb :reversed
   end
 
   get '/friends' do
-    #write your code here
+     @friends = ["Charlie Chaplin", "Richard Pryor", "Eddie Murphy", "Louis CK", "Jerry Seinfeld"]
+     erb :friends
   end
 
 end
